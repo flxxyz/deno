78890d9152a90e7f757c8dc15c91ae0f4f642522
JOYBOY <zzz@alloc.cloud>
2022-08-25 08:34:12 +0000

    Update ReadFileToResponse.ts

--- ReadFileToResponse.ts
+++ ReadFileToResponse.ts
@@ -13,7 +13,13 @@ export const mimeTypes = {
   '.svg': 'image/svg+xml',
   '.woff': 'application/x-font-woff',
   '.woff2': 'font/woff2',
+  '.txt': 'text/plain',
+  '.ico': 'image/x-icon',
 };
+
+export const excludeMimeTypes = [
+  '.map',
+];
 
 export async function ReadFileToResponse(url: string, dir: string) {
     let pathname = new URL(url).pathname;
@@ -21,6 +27,12 @@ export async function ReadFileToResponse(url: string, dir: string) {
       pathname = 'index.html';
     }
     const ext = extname(pathname);
+    if (excludeMimeTypes.includes(ext)) {
+      return new Response(null, {
+        status: Status.NotFound,
+      });
+    }
+
     const mimeType = mimeTypes[ext] || DEFAULT_MIME_TYPE;
     const file = join(dir, pathname);
     const content = await readFile(file);
